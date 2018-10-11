3a0a3f0a1925a261177406447bdde8cd85063b13
YulunW <43308190+YulunW@users.noreply.github.com>
2018-10-11 13:12:36 -0400

    Update OSdetection.py

--- Activity 4/OSdetection.py
+++ Activity 4/OSdetection.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+
 import ping
 
 
@@ -7,11 +8,18 @@ def main():
     input_file = open(file_name, "r")
     ip_addrs = input_file.readlines()
     for ip_addr in ip_addrs:
-        mix = ping.do_one(ip_addr, 3)
+        if ip_addr[-1] == "\n":
+            ip_addr = ip_addr[:-1]
+        try:
+            mix = ping.do_one(ip_addr, 3)
+        except:
+            print "{ip} could not be reached".format(ip=ip_addr)
+            continue
         if mix == None:
+            print "{ip} could not be reached".format(ip=ip_addr)
             continue
         delay, ttl = mix
-        if ttl <= 64:
+        if (ttl > 64) and (ttl<=128):
             print "%s is running Windows" % ip_addr
         else:
             print "%s is running Linux" % ip_addr
